e4b401a3ed0e9ccfc3401707f826fe97419db402
Danielius <Danasltuu@gmail.com>
2023-07-11 21:48:02 +0300

    account_id

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from schemas.account_schemas import AccountCreate
 
 app = FastAPI()
 
-@app.post("/users/", response_model=AccountCreate)
-def create_user(account: AccountCreate):
+@app.post("/accounts/", response_model=AccountCreate)
+def create_account(account: AccountCreate):
     return account.model_dump()
 
 
-@app.get("/users/", response_model=List[AccountCreate])
-def gel_all_users():
+@app.get("/accounts/", response_model=List[AccountCreate])
+def get_all_accounts():
     return [AccountCreate.Config.json_schema_extra["example"]]
+
+@app.get("/accounts/{account_id}")
+async def get_account_by_id(account_id):
+    return {"message": account_id}
